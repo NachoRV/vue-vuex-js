3f2935acf00a9c192bef69e8f8afbe01bbed3ba7
‘Ignacio’ <royovillanova.i@gmail.com>
2018-11-23 13:24:39 +0100

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ titel }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +31,15 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  computed: {
+    ...mapState(['titel']),
   },
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -4,8 +4,27 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+
+  /**
+   * State declaramos las propiedades o variables que queremos almacenar en el Store
+   * en el componente podemos importarlas con:
+   *  import { mapState } from 'vuex';
+   *   computed: {
+    ...mapState(['titel']),
+    },
+   */
   state: {
 
+    titel: 'Hola desde el Store Nacho',
+
+  },
+  /**
+   * los utilizamos para acceder a los datos cuando necesitemos realizar calculos con ellos
+   */
+  getters: {
+    countTitel: state => {
+      return state.titel.length;
+    },
   },
   mutations: {
 
